705652517b48124653cbd6b1bbc32fe63aa833eb
Jaerker <johanjakberger@gmail.com>
2024-09-10 15:50:09 +0200

    Fixed check to avoid multiple room number strings for rooms

--- functions/Rooms/GetRooms/index.js
+++ functions/Rooms/GetRooms/index.js
@@ -2,16 +2,24 @@ const {db} = require('../../../services/index');
 const { response } = require('../../../responses/index');
 exports.handler = async (event) => {
 
+  const {avaliableRoomsOnly} = event?.queryStringParameters;
+
   const { Items } = await db.scan({
     TableName: 'bonz-ai-db',
     FilterExpression: 'attribute_exists(#sk)',
     ExpressionAttributeNames: {
-      '#sk': 'room'
+      '#sk': 'sk'
     }
   });
   if (Items) {
+
+    if(avaliableRoomsOnly === 'true'){
+      return response(200, Items.filter((item) => item.object.isAvaliable === true));
+    }
+
+
     return response(200, Items);
   }
 
-  return response(200, 'ok fungerar i GetRooms, men hittade inga rum');
+  return response(200, event);
 };
